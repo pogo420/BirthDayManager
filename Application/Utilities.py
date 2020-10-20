6df9dc21ea3a42b30ced8c692257191e4959b5cd
pogo420 <am0072008@gmail.com>
2020-10-20 11:48:52 +0530

    error exit condition chnaged

--- Application/Utilities.py
+++ Application/Utilities.py
@@ -2,7 +2,7 @@ import sys
 from typing import Any
 
 
-def print_error(error_object, is_exit=True):
+def print_error(error_object, is_exit=False):
     """Function of printing error message"""
     error_message = f"Issue: {error_object}"
     print(error_message)
