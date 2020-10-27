96c8c054f05952653551f5b29c130f0115c3060e
pogo420 <am0072008@gmail.com>
2020-10-27 19:37:34 +0530

    JwtHelper updates/negative test cases

--- Tests/JwtHelper_tests.py
+++ Tests/JwtHelper_tests.py
@@ -1,3 +1,5 @@
+import pytest
+
 from Application.JwtHelper import JwtHelper
 
 
@@ -21,4 +23,16 @@ def test_token_validate(jwt_details):
 
     validation_str = JwtHelper().set_secret(jwt_details.get("secret")).validate_token(token=token)
     assert validation_str.get("data") == jwt_details.get("data")
+
+
+def test_token_invalidate(jwt_details):
+    """Function for testing jwt invalidation"""
+    token = JwtHelper() \
+        .set_data(jwt_details.get("data")) \
+        .set_secret("rubbish") \
+        .set_expiration(jwt_details.get("exp_duration")) \
+        .generate_token()
+    with pytest.raises(Exception) as exp:
+        validation_str = JwtHelper().set_secret(jwt_details.get("secret")).validate_token(token=token)
+    assert str(exp.value) == "Signature verification failed"
 
