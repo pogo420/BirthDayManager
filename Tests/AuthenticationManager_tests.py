2d7299b8d532977bf4c7b0bad0e37dc1e8f6a020
pogo420 <am0072008@gmail.com>
2020-11-01 19:23:38 +0530

    authentication update + user query handler

---
+++ Tests/AuthenticationManager_tests.py
@@ -0,0 +1,6 @@
+from Application.AuthenticationManager import AuthenticationManager
+
+
+def test_auth_manager():
+    assert_status = AuthenticationManager().authenticate("arnab", "test")
+    assert assert_status == True
--- Tests/UserQueryHandler_tests.py
+++ Tests/UserQueryHandler_tests.py
@@ -3,7 +3,7 @@
 def test_connection(client):
     """Function for testing basic test application"""
     response = client.get("/")
-    assert response.data == b'{"message":"Test Response"}\n'
+    assert response.data == b'Test Response'
 
 
 
