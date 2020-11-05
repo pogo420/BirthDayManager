3f64fad9a21ac1a44de9c346f81b9e500dd0cb73
pogo420 <am0072008@gmail.com>
2020-11-05 21:20:07 +0530

    workflow setup

--- env-setup.sh
+++ env-setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "setting environment variables"
 export SQLITE_DB=Db/bdm_datbase.db
 export DB_SETUP_SCRIPT=Db/SchemaCreate.sql
 export MASTER_TABLE=birthday_data
@@ -8,4 +7,3 @@ export JWT_SECRET=dummy-secret-1
 export JWT_EXPIRE=60
 export FLASK_APP=Application.UserQueryHandler.py
 bash ./Db/db_setup.sh
-echo $SQLITE_DB
